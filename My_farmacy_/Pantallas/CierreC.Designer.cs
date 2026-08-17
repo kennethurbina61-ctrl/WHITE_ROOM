da8102611c26767ff8c04788857262bb72318af1
Edgar3490 <edgarcuadra12@gmail.com>
2026-08-17 14:48:31 -0600

    feature-chamba
    caja

---
+++ My_farmacy_/Pantallas/CierreC.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace My_farmacy_.Pantallas
+{
+    partial class CierreC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelinformacion = new System.Windows.Forms.Panel();
+            this.lblnombrepantalla = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtcorreo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lbldatos = new System.Windows.Forms.Label();
+            this.lblusuarui = new System.Windows.Forms.Label();
+            this.txtArqueo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pcicon = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelinformacion.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcicon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelinformacion
+            // 
+            this.panelinformacion.BackColor = System.Drawing.SystemColors.Control;
+            this.panelinformacion.Controls.Add(this.pcicon);
+            this.panelinformacion.Controls.Add(this.lblnombrepantalla);
+            this.panelinformacion.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelinformacion.Location = new System.Drawing.Point(0, 0);
+            this.panelinformacion.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panelinformacion.Name = "panelinformacion";
+            this.panelinformacion.Size = new System.Drawing.Size(983, 61);
+            this.panelinformacion.TabIndex = 9;
+            // 
+            // lblnombrepantalla
+            // 
+            this.lblnombrepantalla.AutoSize = true;
+            this.lblnombrepantalla.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombrepantalla.Location = new System.Drawing.Point(98, 12);
+            this.lblnombrepantalla.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblnombrepantalla.Name = "lblnombrepantalla";
+            this.lblnombrepantalla.Size = new System.Drawing.Size(275, 32);
+            this.lblnombrepantalla.TabIndex = 1;
+            this.lblnombrepantalla.Text = "ARQUEO DE CAJA";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.txtcorreo);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtUsuario);
+            this.panel1.Controls.Add(this.lbldatos);
+            this.panel1.Controls.Add(this.lblusuarui);
+            this.panel1.Controls.Add(this.txtArqueo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.panel1.Location = new System.Drawing.Point(23, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(922, 180);
+            this.panel1.TabIndex = 10;
+            // 
+            // txtcorreo
+            // 
+            this.txtcorreo.BackColor = System.Drawing.Color.Silver;
+            this.txtcorreo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtcorreo.Location = new System.Drawing.Point(295, 99);
+            this.txtcorreo.Name = "txtcorreo";
+            this.txtcorreo.Size = new System.Drawing.Size(179, 24);
+            this.txtcorreo.TabIndex = 28;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 18);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Hora:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Silver;
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsuario.Location = new System.Drawing.Point(579, 99);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(257, 24);
+            this.txtUsuario.TabIndex = 13;
+            // 
+            // lbldatos
+            // 
+            this.lbldatos.AutoSize = true;
+            this.lbldatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldatos.ForeColor = System.Drawing.Color.Blue;
+            this.lbldatos.Location = new System.Drawing.Point(8, 12);
+            this.lbldatos.Name = "lbldatos";
+            this.lbldatos.Size = new System.Drawing.Size(213, 25);
+            this.lbldatos.TabIndex = 10;
+            this.lbldatos.Text = "Información General:";
+            // 
+            // lblusuarui
+            // 
+            this.lblusuarui.AutoSize = true;
+            this.lblusuarui.Location = new System.Drawing.Point(25, 63);
+            this.lblusuarui.Name = "lblusuarui";
+            this.lblusuarui.Size = new System.Drawing.Size(59, 18);
+            this.lblusuarui.TabIndex = 9;
+            this.lblusuarui.Text = "Fecha:";
+            // 
+            // txtArqueo
+            // 
+            this.txtArqueo.BackColor = System.Drawing.Color.Silver;
+            this.txtArqueo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArqueo.Location = new System.Drawing.Point(28, 99);
+            this.txtArqueo.Name = "txtArqueo";
+            this.txtArqueo.Size = new System.Drawing.Size(172, 24);
+            this.txtArqueo.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(655, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Usuario:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.panel2.Location = new System.Drawing.Point(23, 262);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(922, 299);
+            this.panel2.TabIndex = 11;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(8, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Informe del día:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ventas totales:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Silver;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(90, 99);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 41);
+            this.textBox3.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ErrorImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pcicon
+            // 
+            this.pcicon.BackgroundImage = global::My_farmacy_.Properties.Resources.icono_clientes;
+            this.pcicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pcicon.Location = new System.Drawing.Point(12, 6);
+            this.pcicon.Name = "pcicon";
+            this.pcicon.Size = new System.Drawing.Size(76, 46);
+            this.pcicon.TabIndex = 4;
+            this.pcicon.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.ErrorImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox3.Location = new System.Drawing.Point(331, 184);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox3.TabIndex = 38;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(392, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 18);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Total neto del día:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(404, 230);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 41);
+            this.textBox2.TabIndex = 36;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.ErrorImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox4.Location = new System.Drawing.Point(605, 52);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox4.TabIndex = 35;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(655, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 18);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "Egresos:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Silver;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Location = new System.Drawing.Point(667, 98);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(88, 41);
+            this.textBox4.TabIndex = 33;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(404, 99);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 41);
+            this.textBox1.TabIndex = 30;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.ErrorImage = global::My_farmacy_.Properties.Resources.carrito;
+            this.pictureBox2.Location = new System.Drawing.Point(342, 53);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 18);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Ingresos:";
+            // 
+            // CierreC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 670);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelinformacion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CierreC";
+            this.Text = "CierreC";
+            this.panelinformacion.ResumeLayout(false);
+            this.panelinformacion.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcicon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelinformacion;
+        private System.Windows.Forms.PictureBox pcicon;
+        private System.Windows.Forms.Label lblnombrepantalla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtcorreo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lbldatos;
+        private System.Windows.Forms.Label lblusuarui;
+        private System.Windows.Forms.TextBox txtArqueo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
